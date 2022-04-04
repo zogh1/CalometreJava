d43f432bce9bac340dadd591312d65581670a4d1
Ahmed <ahmed.mahjoub@esprit.tn>
2022-04-04 23:44:27 +0100

    Vintegration

--- src/sevice/RecetteService.java
+++ src/sevice/RecetteService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package service;
+package sevice;
 
 import interfaces.RecetteInterface;
 import java.sql.Connection;
--- src/sevice/ServiceEvent.java
+++ src/sevice/ServiceEvent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package services;
+package sevice;
 
 import java.sql.PreparedStatement;
 
