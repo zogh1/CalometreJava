7ea9e81b03523b0aa2859679aba05a3324d795e5
Ahmed <ahmed.mahjoub@esprit.tn>
2022-04-04 22:08:35 +0100

    Crud+Entité

---
+++ src/interfaces/RecetteInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.List;
+import model.Recette;
+
+/**
+ *
+ * @author Ahmed Mahjoub
+ */
+public interface RecetteInterface {
+     public void addRecette(Recette a);
+    public List<Recette> fetchAllRecette();
+    public void DeleatRecette(int id);
+     public void UpdateRecette(Recette a ,int id);
+    
+}
