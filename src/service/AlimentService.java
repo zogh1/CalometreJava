dee3cba9bc38474c0c0959705f795b108e17d7cc
Ahmed <ahmed.mahjoub@esprit.tn>
2022-04-04 23:45:11 +0100

    vfinal

--- src/service/AlimentService.java
+++ src/service/AlimentService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sevice;
+package service;
 
 import interfaces.AlimentInterface;
 import java.sql.Connection;
