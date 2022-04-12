cf958de285bb37e315a07d504b2ffb20e1fc29c6
Zebecks <zoghlami.wassim@esprit.tn>
2022-04-12 03:20:23 +0100

    Aplly done

--- src/calometre/Calometre.java
+++ src/calometre/Calometre.java
@@ -20,18 +20,8 @@ public class Calometre {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ServiceEvent se = new ServiceEvent();
-      
-        Event e = se.getEventById(17) ;
-        System.out.println(e);
-        //   Event e = new Event("New Event","02/22/2022","02/23/2022","Nullam iaculis lacus non lectus venenatis tincidunt. Pellentesque est lectus, semper vitae aliquam in, pretium at libero. Nulla sed mauris neque. Nam eget pulvinar turpis, nec iaculis diam. Proin nec cursus libero",10,"moknin","QSVDJEAC41J2G5X4NJ.jpg");
-        Post p = new Post ("New post","2022-03-22 ","aaaa wassim kifeh",e) ; 
-//se.getallEvent();
-     ServicePost sp = new ServicePost(); 
-     // sp.getallPost(); 
-      //se.getEventById(17);
-      // se.createEvent(e);
-      sp.createPost(p) ;  
+    ServiceEvent se = new ServiceEvent();
+        se.applyToEvent(6, 17);
     }
     
 }
